c910a759dfa9411b3f880a71fa8a0fa02bd79404
Yanjie He <heyanjie0@outlook.com>
2019-03-16 22:13:08 -0400

    Updates

---
+++ precache-manifest.7d68683cc16dfd3c597bc325e4924fcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c267e5f594f62e341364",
+    "url": "/minimum-steps-to-reach-a-target-by-a-knight/static/css/main.27bccccb.chunk.css"
+  },
+  {
+    "revision": "c267e5f594f62e341364",
+    "url": "/minimum-steps-to-reach-a-target-by-a-knight/static/js/main.ef7a150b.chunk.js"
+  },
+  {
+    "revision": "11902d8ed9439304e786",
+    "url": "/minimum-steps-to-reach-a-target-by-a-knight/static/js/runtime~main.2d33473e.js"
+  },
+  {
+    "revision": "b43ba2d76bdc02655c76",
+    "url": "/minimum-steps-to-reach-a-target-by-a-knight/static/js/2.67b98335.chunk.js"
+  },
+  {
+    "revision": "356a4b3d2ddbac18ee775040f7fd88ff",
+    "url": "/minimum-steps-to-reach-a-target-by-a-knight/index.html"
+  }
+];
